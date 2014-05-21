738792012b699b547267f411f6a195efc4e12d1d
Michael Han <mike.han@saucelabs.com>
2014-05-20 22:52:36 -0700

    Add some more options for vnc-build
    -w to build the wrapper lib (will not build by default)
    -s to skip deploy to device.

--- vnc-build.sh
+++ vnc-build.sh
@@ -9,7 +9,12 @@ DEPLOY_PATH=/data/local
 CWD=$(pwd)
 
 android=18
-usage="usage: $0 [-a N] -w"
+usage="usage: $0 [-a N] -w -s"
+
+clean() {
+    rm ${LIB_BUILD_PATH}/libdvnc_flinger_sdk${android}.so 2>/dev/null
+    rm ${DAEMON_BUILD_PATH}/*
+}
 
 build_wrapper() {
     cd ../src
@@ -19,20 +24,33 @@ build_wrapper() {
     cd ${CWD}
 }
 
-while getopts ":a:w" opt; do
+deploy_vnc() {
+    adb push ${LIB_BUILD_PATH}/libdvnc_flinger_sdk${android}.so ${PUSH_PATH}/libdvnc_flinger_sdk.so
+    adb push ${DAEMON_BUILD_PATH}/androidvncserver ${PUSH_PATH}/androidvncserver
+
+    adb shell "su -c 'cp ${PUSH_PATH}/* ${DEPLOY_PATH}/.'"
+
+    adb shell "su -c 'chmod 777 ${DEPLOY_PATH}/androidvncserver'"
+    adb shell "su -c 'chmod 644 ${DEPLOY_PATH}/libdvnc_flinger_sdk.so'"
+}
+
+while getopts ":a:ws" opt; do
     case $opt in
         a  ) android=$OPTARG ;;
-        w  ) build_wrapper ;;
+        w  ) do_build_wrapper='yep' ;;
+        s  ) skip_deploy='yep' ;;
         \? ) echo $usage
            exit 1 ;;
     esac
 done
 
+clean
 ndk-build
-adb push ${LIB_BUILD_PATH}/libdvnc_flinger_sdk${android}.so ${PUSH_PATH}/libdvnc_flinger_sdk.so
-adb push ${DAEMON_BUILD_PATH}/androidvncserver ${PUSH_PATH}/androidvncserver
 
-adb shell "su -c 'cp ${PUSH_PATH}/* ${DEPLOY_PATH}/.'"
+if [ -n "$do_build_wrapper" ]; then
+    build_wrapper
+fi
 
-adb shell "su -c 'chmod 777 ${DEPLOY_PATH}/androidvncserver'"
-adb shell "su -c 'chmod 644 ${DEPLOY_PATH}/libdvnc_flinger_sdk.so'"
+if [ -z "$skip_deploy" ]; then
+    deploy_vnc
+fi
